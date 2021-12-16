cbc98486e979399418203ae5461ab1520dd4b4f9
Renato Regalado <renato.regalado@gmail.com>
2021-12-16 14:55:47 -0500

    fixed stuff

--- Test-DnsPropagation.ps1
+++ Test-DnsPropagation.ps1
@@ -1,6 +1,6 @@
 param
 (
-    [string]$Zone = "Contoso.com",
+    [string]$Zone = "ironbow.support",
     [string]$ResultsPath = ".\"
 )
 
@@ -9,44 +9,58 @@ import-module DnsClient
 
 
 function test-Record ($Rec) {
-    $recData = $Rec.Data
+    $recData = $Rec.RecordData.IPv4Address.IPAddressToString
 
-    $Test = Test-Connection $recData -Ping -Count 1 -TimeoutSeconds 1
-
-    if ($Test.Status -ne "TimedOut"){
+    if ((test-connection $recData -count 1 -ErrorAction SilentlyContinue).Status -ne "TimedOut"){
         return $true
+    }
+
+    else {
+        return $false
     }
 }
 
 
 function main {
     $Records = Get-DnsServerResourceRecord -ZoneName $Zone
-    $Successful = @()
-    $Failed = @()
+    $global:Successful = New-Object System.Collections.ArrayList
+    $global:Failed = New-Object System.Collections.ArrayList
 
     $script = {
-        if ($rec.RecordType -eq "A"){
-            if (test-Record($Rec)){
-                $Successful += $Rec
+        param ($rec)
+        $recData = $Rec.RecordData.IPv4Address.IPAddressToString
+        $recObject = New-Object -TypeName PSObject -Property ([Ordered] @{
+            'Hostname' = $rec.Hostname
+            'RecordType' = $rec.RecordType
+            'Timestamp' = $rec.Timestamp
+            'TTL' = $rec.TimeToLive
+            'IpAddress' = $recData
+        })
+
+            if ((Test-Connection $recData -count 1 -ErrorAction SilentlyContinue).Status -ne "TimedOut"){
+                write-host -ForegroundColor Green $recObject
+                $global:Successful.Add($recObject)
             }
             else {
-                $Failed += $Rec
+                write-host -ForegroundColor Red $recOBject
+                $global:Failed.Add($recObjec)
+            }
+    }
+
+    foreach ($Rec in $Records) {
+        if ($Rec.RecordType -eq "A"){
+            #& $script -rec $Rec
+            Start-Job -ScriptBlock ($script -rec $args) -ArgumentList $Rec 
+            $RunningJobs = (Get-Job | ? {($_.State -eq "Running") -or ($_.State -eq "NotStarted")}).count
+            While($runningJobs -ne 0){
+                $runningJobs = (Get-Job | ? {($_.State -eq "Running") -or ($_.State -eq "NotStarted")}).count
             }
         }
-    }
-    foreach ($Rec in $Records) {
-        Start-ThreadJob -ScriptBlock $script -InputObject $Rec 
-        $RunningJobs = (Get-Job | ? {($_.State -eq "Running") -or ($_.State -eq "NotStarted")}).count
-        While($runningJobs -ne 0){
-            $runningJobs = (Get-Job | ? {($_.State -eq "Running") -or ($_.State -eq "NotStarted")}).count
-        }
 
-        Get-Job | Remove-Job
-    }
 
-    $Successful = Export-Csv -Path ($ResultsPath + "Successful.csv")
-    $failed = Export-Csv -Path ($ResultsPath+"Failed.csv")
+    $Successful | Export-Csv -Path ($ResultsPath + "Successful.csv")
+    $failed | Export-Csv -Path ($ResultsPath+"Failed.csv")
+    }
 }
-
 #Entry point
 main
