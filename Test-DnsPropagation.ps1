89c5322fecbb4b77f684d79e16e34f83d299b213
Renato Regalado <renato.regalado@gmail.com>
2021-12-16 16:47:23 -0600

    rewrote everything again

--- Test-DnsPropagation.ps1
+++ Test-DnsPropagation.ps1
@@ -7,35 +7,10 @@ param
 import-module DnsServer
 import-module DnsClient
 
-$threads = 1000 # how many simultanious threads. I've tested up to 1000 ok against ~3600 local IPs, ~900 active.
-
+$results = @()
 $Records = Get-DnsServerResourceRecord -ZoneName $Zone
 
-clear
-
-""
-write-host "       Threads: " -nonewline -foregroundcolor yellow
-$threads
-"    Build Pool: "
-"    Drain Pool: "
-" ---------------------"
-write-host "   Total Hosts: $($list.count)"
-write-host "   Alive Hosts: "
-write-host "    Dead Hosts: "
-
-# BLOCK 1: Create and open runspace pool, setup runspaces array with min and max threads
-$pool = [RunspaceFactory]::CreateRunspacePool(1, $threads)
-$pool.ApartmentState = "MTA"
-$pool.Open()
-$runspaces = $results = @()
-
-# --------------------------------------------------
-    
-# BLOCK 2: Create reusable scriptblock. This is the workhorse of the runspace. Think of it as a function.
-$scriptblock = {
-    Param (
-    [string]$rec
-    )
+function new-Record ($rec) {
     $ip = $Rec.RecordData.IPv4Address.IPAddressToString
     $recObjet = [PSCustomObject] [ordered] @{
         Hostname = $rec.Hostname
@@ -48,59 +23,13 @@ $scriptblock = {
     return $recObject
 }
 
-# --------------------------------------------------
-# BLOCK 3: Create runspace and add to runspace pool
-$counter=0
 foreach ($rec in $Records) {
- 
-    $runspace = [PowerShell]::Create()
-    $null = $runspace.AddScript($scriptblock)
-    $null = $runspace.AddArgument($rec)
-
-    $runspace.RunspacePool = $pool
- 
-# BLOCK 4: Add runspace to runspaces collection and "start" it
-    # Asynchronously runs the commands of the PowerShell object pipeline
-    $runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
-
-	$Host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates 16 , 2
-	$counter++
-	write-host "$counter " -nonewline
+    $results += new-Record (rec)
 }
-
-# --------------------------------------------------
- 
-# BLOCK 5: Wait for runspaces to finish
-
-$total=$counter
-$counter=0
-
-# BLOCK 6: Clean up
-foreach ($runspace in $runspaces ) {
-    # EndInvoke method retrieves the results of the asynchronous call
-    $results += $runspace.Pipe.EndInvoke($runspace.Status)
-    $runspace.Pipe.Dispose()
-	
-	$Host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates 16 , 3
-	$counter++
-	write-host "$($total-$counter) " -nonewline
-
-}
-    
-$pool.Close() 
-$pool.Dispose()
-
-# --------------------------------------------------
-
-# Bonus block 7
-# Look at $results to see any errors or whatever was returned from the runspaces
-
-# Use this to output to JSON. CSV works too since it's simple data.
-$results | ConvertTo-Csv > ($ResultsPath+"records.csv")
 
 $total=$results.count
 $alive = $($results | ? {$_.State -eq "true"}).count
-$dead = $($results | ? {$_.State -ne "true"}).count
+$dead = $($results | ? {$_.State-ne "true"}).count
 
 $Host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates 0 , 5
 
@@ -115,4 +44,4 @@ $dead
 
 ""
 
-$results | ? {$_.State -eq "true"} | select ip,DNS,MAC
+$results | ? {$_.State-ne "true"}
